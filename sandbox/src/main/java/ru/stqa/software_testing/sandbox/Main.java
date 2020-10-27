08d6988bd33d579be99465c37e172ebd3dc90f59
Kirill-Prokhorov <73255618+Kirill-Prokhorov@users.noreply.github.com>
2020-10-27 10:57:44 +0300

    создан класс Square

--- sandbox/src/main/java/ru/stqa/software_testing/sandbox/Main.java
+++ sandbox/src/main/java/ru/stqa/software_testing/sandbox/Main.java
@@ -2,20 +2,21 @@ package ru.stqa.software_testing.sandbox;
 
 public class Main {
 
-  public static void main(String[] args){
-    
-	System.out.println("Hello, World!");
+	public static void main(String[] args) {
 
-	Point p1 = new Point(7, 9);
-	Point p2 = new Point(16, 10);
-	  System.out.println(distance(p1, p2));
-	  System.out.println(p1.distance(p2));
+		System.out.println("Hello, World!");
+
+		Point p1 = new Point(7, 9);
+		Point p2 = new Point(16, 10);
+		System.out.println(distance(p1, p2));
+		System.out.println(p1.distance(p2));
 		System.out.println(p2.distance(p1));
 
-  }
 
-    public static double distance(Point p1, Point p2){
+	}
 
-        return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
-    }
+	public static double distance(Point p1, Point p2) {
+
+		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+	}
 }
---
+++ sandbox/src/main/java/ru/stqa/software_testing/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.software_testing.sandbox;
+
+public class Square {
+  double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+  public double area(){
+    return this.l * this.l;
+  }
+
+}
