da5143f8b9d3811084257290b4a0a1c9d36c1d43
Kirill-Prokhorov <73255618+Kirill-Prokhorov@users.noreply.github.com>
2020-11-06 21:17:44 +0300

    Создание теста модификации групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/software_testing/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.software_testing.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.software_testing.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void groupModification() {
+
+    application.getNavigationHelper().gotoGroupPage();
+    application.getGroupHelper().selectGroup();
+    application.getGroupHelper().initGroupModification();
+    application.getGroupHelper().fillGroupForm(new GroupData("change1", "change2", "change3"));
+    application.getGroupHelper().submitGroupModification();
+    application.getGroupHelper().returnToGroupPage();
+  }
+
+}
