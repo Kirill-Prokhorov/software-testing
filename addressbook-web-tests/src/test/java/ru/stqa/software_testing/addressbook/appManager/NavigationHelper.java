0155bfb353225b08cea3d6c265edb1f6faf0bd8d
Kirill-Prokhorov <73255618+Kirill-Prokhorov@users.noreply.github.com>
2020-11-07 23:49:00 +0300

    Настроена проверка необходимости перехода между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/software_testing/addressbook/appManager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/software_testing/addressbook/appManager/NavigationHelper.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
+
 public class NavigationHelper extends HelperBase {
 
 
@@ -13,9 +15,18 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
+    if(isElementPresent(By.tagName("h1")) && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+    && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
+
+
   public void gotoHomePage() {
+    if(isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
