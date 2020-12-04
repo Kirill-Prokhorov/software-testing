b79e34436b5cae15db4d52a638ca9433b5302fd7
Kirill-Prokhorov <73255618+Kirill-Prokhorov@users.noreply.github.com>
2020-12-04 23:59:50 +0300

    Начали описывать конфигурационный стенд. build gradle не хочет компилироваться с настройками браузера

--- addressbook-web-tests/src/test/java/ru/stqa/software_testing/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/software_testing/addressbook/tests/TestBase.java
@@ -1,16 +1,15 @@
 package ru.stqa.software_testing.addressbook.tests;
 
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.software_testing.addressbook.appManager.ApplicationManager;
 
 
-
-
 public class TestBase {
 
-  public static final ApplicationManager application = new ApplicationManager("chrome");
+  protected static final ApplicationManager application = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
